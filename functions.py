5f5996625a6ca042f40b38c93e8e4647e327834f
makingmusic <harinder@gmail.com>
2023-05-19 10:48:03 -0400

    commiting  the first Lines of code

---
+++ functions.py
@@ -0,0 +1,4 @@
+def hello_world1():
+    print("Hello World from functions.py!")
+
+print("I am functions.py")
---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+import json
+import functions as f
+
+
+def hello_world():
+    print("Hello World!")
+
+#f.hello_world1()
+
+f.hello_world1()
+hello_world()
+
+
