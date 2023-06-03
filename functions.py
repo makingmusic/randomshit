625a66acf905350a6db249017ea940eef8d4d193
makingmusic <harinder@gmail.com>
2023-06-03 08:47:58 -0400

    built a simple function that returns the json formatted time

--- functions.py
+++ functions.py
@@ -1,4 +1,20 @@
+import datetime
+import json
+
 def hello_world():
     print("Hello World from functions.py!")
+
+def return_time():
+    time = datetime.datetime.now()
+    time_obj = {
+        "day": time.strftime("%A"),
+        "date": time.strftime("%d %B %Y"),
+        "time": time.strftime("%H:%M:%S"),
+        "ap": time.strftime("%p")
+    }
+    #time_obj_json = json.dumps(time_obj, indent=2)
+    #time_obj_html = "<html><body><pre>" + time_obj_json + "</pre></body</html>"
+    #print ("returning this string: \n" + time_obj_html)
+    return time_obj
 
 
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests
+#import requests
 import json
 import functions as f
 from fastapi import FastAPI
@@ -8,6 +8,12 @@ f.hello_world()
 app = FastAPI()
 
 @app.get("/")
-
 async def root():
     return {"message": "Hello World How are you?"}
+
+@app.get("/time")
+async def time():
+    t = f.return_time()
+    t_json = json.dumps(t, indent=4, default=str)
+    print ("returning :\n" + t_json)
+    return t_json
